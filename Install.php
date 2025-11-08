b785fdaa080e0fca93fdbe74dbb53842b9d04f33
Shcno <mcgroarty.l@oundleschool.org.uk>
2025-11-08 12:27:22 +0000

    tblusers

--- Install.php
+++ Install.php
@@ -8,4 +8,20 @@
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     $sql="CREATE DATABASE IF NOT EXISTS Sandwiches"
     $connn->exec($sql);
+    $sql='USE Sandwiches'
+    echo("DB made")
+    $stmt1= $conn->prepare("DROP TABLE IF EXISTS tblusers;
+    
+    CREATE TABLE tblusers
+    (UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(20) NOT NULL,
+    Surname VARCHAR(20) NOT NULL,
+    Forename VARCHAR(20) NOT NULL,
+    Password VARCHAR(200) NOT NULL,
+    Year INT(2) NOT NULL,
+    Balance DECIMAL(15,2) NOT NULL,
+    ROLE TINYINT(1));
+    ");
+
+    $stmt1->execute();
     ?>
