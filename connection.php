3dd8893a594e70f5355d564203d0a1dffff882d7
Shcno <mcgroarty.l@oundleschool.org.uk>
2025-11-13 09:32:35 +0000

    Proper Connect

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@
     $password="Password";
     $dbname="Sandwiches"
 
-    $conn=new PDO("mysql:host=$servername",$username,$password);
+    $conn=new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     echo("connected ok")
 ?>
