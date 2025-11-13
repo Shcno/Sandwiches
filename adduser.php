aaef5147eab3148d18aa8e2a6aac4670d6b4359e
Shcno <mcgroarty.l@oundleschool.org.uk>
2025-11-13 09:47:46 +0000

    my god

--- adduser.php
+++ adduser.php
@@ -1,4 +1,25 @@
 <?php
     print_r($_POST);
     include_once("connection.php");
+    $username="bob";
+    $role=1;
+
+    $stmt1= $conn->prepare("INSERT INTO tblusers
+    (UserID, Username, Surname, Forename, Password, Year, Balance, ROLE);
+    VALUES
+    (NULL,:Username, :Surname, :Forename, :Password, :Year, :Balance, :Role)
+    ");
+
+    $stmt1->bindParam(":Username",$username);
+    $stmt1->bindParam(":Surname",$_POST["surname"]);
+    $stmt1->bindParam(":Forename",$forename);
+    $stmt1->bindParam(":Password",$password;
+    $stmt1->bindParam(":Username",$username);
+    $stmt1->bindParam(":Username",$username);
+    $stmt1->bindParam(":Username",$username);
+    $stmt1->bindParam(":Username",$username);
+
+    $stmt1->execute();
 ?>
+
+#STRC 12
--- connection.php
+++ connection.php
@@ -7,4 +7,9 @@
     $conn=new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     echo("connected ok");
+    try{
+        $conn=new("mysql:host=$servername;dbname=$dbname",$username,$password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        echo
+    }
 ?>
